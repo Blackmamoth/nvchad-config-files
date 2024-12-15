7ec55fc84162314274c1b66983c741dde36cff28
blackmamoth <blackmamoth83@gmail.com>
2024-12-15 22:46:49 +0530

    removed '--rm-unused' arg from goimports_revisor

--- lua/configs/null-ls-go.lua
+++ lua/configs/null-ls-go.lua
@@ -4,9 +4,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local opts = {
   sources = {
     null_ls.builtins.formatting.gofumpt,
-    null_ls.builtins.formatting.goimports_reviser.with({
-      extra_args = {"--rm-unused"}
-    }),
+    null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
   },
   on_attach = function (client, bufnr)
